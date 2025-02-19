de604a28bc4a7873b2d76b8631082bf66b420775
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2025-02-19 10:09:57 +0530

    created_PostsModel_createdPostSchema_Dt:19022025

---
+++ models/Posts.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    },
+  ],
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Post = mongoose.model("post", postSchema);
+
+export default Post;
