f30a3a5ce9a55f61ad022fd45f3bce9de74886e2
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2025-02-24 00:18:13 +0530

    created_Register&LoginAuthScreens_Dt:24022025

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,114 @@
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
+// Defining the state for Register Functionality
 const Register = () => {
-  return <div>Register</div>;
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const { name, email, password, confirmPassword } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    if (password === confirmPassword) {
+      // if not using redux, we can use the APIs in this way and update the data accordingly.
+      // try {
+      //   const newUser = {
+      //     name,
+      //     email,
+      //     password,
+      //   };
+
+      //   const config = {
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //     },
+      //   };
+
+      //   const body = JSON.stringify(newUser);
+
+      //   const res = await axios.post("/api/users/register", body, config);
+      //   console.log(res.data);
+      // } catch (error) {
+      //   console.log(error.message);
+      // }
+
+      console.log("SUCCESS");
+    } else {
+      console.log("password does not matches");
+    }
+  };
+
+  return (
+    <>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Create Your Account
+      </p>
+      <form onSubmit={onSubmit} className="form" action="create-profile.html">
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            required
+            value={name}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            required
+            value={email}
+            onChange={(e) => onChange(e)}
+          />
+          <small className="form-text">
+            This site uses Gravatar so if you want a profile image, use a
+            Gravatar email
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            required
+            value={password}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            minLength="6"
+            required
+            value={confirmPassword}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Login</Link>
+      </p>
+    </>
+  );
 };
 
 export default Register;
