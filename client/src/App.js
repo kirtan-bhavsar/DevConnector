1e33201b42fe4121d617fada14d0319302280ff4
kirtan-bhavsar <kirtanbhavsar.2491@gmail.com>
2025-02-23 18:31:12 +0530

    created_Login&AuthComponents_createdBasicBody_Dt:23022025

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,12 @@ import Navbar from "./components/layouts/Navbar.js";
 import Landing from "./components/layouts/Landing.js";
 import Login from "./components/auth/Login.js";
 import Register from "./components/auth/Register.js";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Switch,
+} from "react-router-dom";
 
 const App = () => {
   return (
@@ -11,6 +16,17 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Landing />} />
+        <Route
+          path="/*"
+          element={
+            <section className="container">
+              <Routes>
+                <Route path="/register" element={<Register />}></Route>
+                <Route path="/login" element={<Login />}></Route>
+              </Routes>
+            </section>
+          }
+        ></Route>
       </Routes>
     </Router>
   );
